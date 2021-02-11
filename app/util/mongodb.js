e937a1b9f3810762721867391c925c270c00a231
Benny Blockchain <ben@styng.social>
2021-02-10 23:24:51 -0500

    Connect to MongoDB

---
+++ app/util/mongodb.js
@@ -0,0 +1,31 @@
+import { MongoClient } from "mongodb";
+import { MONGO_URI, DB_NAME } from "../config/mongo.config";
+if (!MONGO_URI) {
+  console.log(MONGO_URI);
+  throw new Error("No MongoDB connection link provided.");
+}
+
+if (!DB_NAME) {
+  throw new Error("No database name provided.");
+}
+
+let cachedClient = null;
+let cachedDb = null;
+
+export async function connectToDatabase() {
+  if (cachedClient && cachedDb) {
+    return { client: cachedClient, db: cachedDb };
+  }
+
+  const client = await MongoClient.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const db = await client.db(DB_NAME);
+
+  cachedClient = client;
+  cachedDb = db;
+
+  return { client, db };
+}
