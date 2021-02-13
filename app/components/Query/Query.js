9dee818f7599d76472f9083f17cce6cf97c81cd7
Benny Blockchain <ben@styng.social>
2021-02-12 19:05:29 -0500

    Documentation

--- app/components/Query/Query.js
+++ app/components/Query/Query.js
@@ -1,10 +1,20 @@
+// Component dependencies
 import { Modal, Button } from "react-bootstrap";
-import styles from "./Query.module.scss";
+import styles from "./Query.module.css";
+
+/**
+ * Query
+ * * Example of Better Comments extension.
+ * ! Warning comment
+ * ? Question
+ * TODO: Add todo comments
+ * @param props Document parama
+ */
 
 const Query = ({ courseID, professor, team, project }) => {
   return (
     <>
-      <Modal show={true} centered>
+      <Modal show={true} className={styles.modal} centered>
         <Modal.Header closeButton>
           <Modal.Title>
             <h3>Silver Surfer DB Query</h3>
@@ -15,15 +25,14 @@ const Query = ({ courseID, professor, team, project }) => {
             db: silver-mongo <br />
             collection: CoursePages
           </p>
-
-          <h5 className={styles.header}>Course</h5>
-          <p className={styles.paragraph}>{courseID}</p>
-          <h5 className={styles.header}>Professor</h5>
-          <p className={styles.paragraph}>{professor}</p>
-          <h5 className={styles.header}>Team</h5>
-          <p className={styles.paragraph}>{team}</p>
-          <h5 className={styles.header}>Project Name</h5>
-          <p className={styles.paragraph}>{project}</p>
+          <h5>Course</h5>
+          <p>{courseID}</p>
+          <h5>Professor</h5>
+          <p>{professor}</p>
+          <h5>Team</h5>
+          <p>{team}</p>
+          <h5>Project Name</h5>
+          <p>{project}</p>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary">Close</Button>
