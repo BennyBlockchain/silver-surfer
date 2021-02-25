f94d02e6d93399430e4b4269713ae35d50a6dacc
Marisa Stover <stoverme2021@mountunion.edu>
2021-02-25 10:11:22 -0500

    created course.js file in paths folder

---
+++ app/pages/course.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import { Modal, Button, Row } from "react-bootstrap";
+import Main from "../layouts/Main/Main";
+
+export default function Home(props) { 
+    return (
+    <head>
+        <title>Silver Surfer</title>
+    </head>
+    
+    );
+}
+
