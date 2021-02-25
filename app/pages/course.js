0fcce5bdaf5572249b145f58f4c320e89a9b7b60
Marisa Stover <stoverme2021@mountunion.edu>
2021-02-25 10:45:03 -0500

    removed most imports from course page

--- app/pages/course.js
+++ app/pages/course.js
@@ -1,13 +1,20 @@
-import Head from "next/head";
+
 import { Modal, Button, Row } from "react-bootstrap";
-import Main from "../layouts/Main/Main";
+
+
 
 export default function Home(props) { 
     return (
+    <>
     <head>
         <title>Silver Surfer</title>
     </head>
-    
+
+    <Row>
+        <p> This is the Course page.</p>
+    </Row>
+   
+    </> 
     );
 }
 
