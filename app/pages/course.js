b9ae7704d6bef47f87fe4ce09760507ec1d32fbe
Marisa Stover <stoverme2021@mountunion.edu>
2021-02-28 19:04:08 -0500

    Made a few corrections and simplified

--- app/pages/course.js
+++ app/pages/course.js
@@ -2,7 +2,7 @@ import { Modal, Button, Row } from "react-bootstrap";
 import styles from "../styles/Home.module.scss";
 
 
-export default function Home(props) { 
+export default function Course() { 
     return (
     <>
     <head>
