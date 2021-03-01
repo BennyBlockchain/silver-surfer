af4a9640b11d08e5aa916c7874ff5c1194be1ba6
Marisa Stover <stoverme2021@mountunion.edu>
2021-03-01 16:40:11 -0500

    added "Course"

--- app/pages/course/[ID].js
+++ app/pages/course/[ID].js
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 const CoursePage = () => {
   const router = useRouter();
   const { ID } = router.query
-  return <div> { ID } </div>;
+  return <div> Course { ID } </div>;
   
 }
 
