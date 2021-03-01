8370bfa1445a2b162422f54f9ef591e1b6e1b50f
Marisa Stover <stoverme2021@mountunion.edu>
2021-02-28 19:37:25 -0500

    updated dynamic document routing

--- app/pages/course/[ID].js
+++ app/pages/course/[ID].js
@@ -2,16 +2,22 @@ import { Modal, Button, Row } from "react-bootstrap";
 import styles from "../../styles/Home.module.scss";
 
 
-export default function Course() { 
+export default function ID() { 
     return (
     
-        <p>Course Page</p>
+        <p>ID page</p>
    
     );
 }
 
 export async function getStaticPaths() { 
-
+    return { 
+        paths: [
+            '/course/csc120.js', 
+            { params: {slug: 'csc120'}}
+        ],
+        fallback: true, 
+    }
 }
 
 export async function getStaticProps({params}) { 
