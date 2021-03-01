3067c9f09fa9e6f1cecc99b2bbf4d2ea9853727c
Marisa Stover <stoverme2021@mountunion.edu>
2021-02-28 19:37:34 -0500

    created csc120.js page

---
+++ app/pages/course/csc120.js
@@ -0,0 +1,18 @@
+import { Modal, Button, Row } from "react-bootstrap";
+import styles from "../../styles/Home.module.scss";
+
+
+export default function csc120() { 
+    return (
+    <>
+    <head>
+        <title>Silver Surfer</title>
+    </head>
+
+    <Row className="mx-0">
+    <p className={`${styles.text}`}>  This is the 120 course page.</p>
+    </Row>
+   
+    </> 
+    );
+}
