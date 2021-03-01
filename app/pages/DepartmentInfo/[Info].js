42c7c4d45a26bc5ee395d3423bbcaa62df46bdea
Marisa Stover <stoverme2021@mountunion.edu>
2021-02-28 19:10:38 -0500

    created [info].js page to begin dynamic routing

---
+++ app/pages/DepartmentInfo/[Info].js
@@ -0,0 +1,19 @@
+import { Modal, Button, Row } from "react-bootstrap";
+import styles from "../../styles/Home.module.scss";
+
+
+export default function Course() { 
+    return (
+    
+        <p>Info Pages</p>
+   
+    );
+}
+
+export async function getStaticPaths() { 
+
+}
+
+export async function getStaticProps({params}) { 
+    
+}
