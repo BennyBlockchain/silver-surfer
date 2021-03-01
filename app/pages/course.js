7090c722c3ed194444f7063e0fc7a430bd7257c7
Marisa Stover <stoverme2021@mountunion.edu>
2021-03-01 11:56:36 -0500

    updated course page + moved it for dynamic routing

--- app/pages/course.js
+++ app/pages/course.js
@@ -1,19 +1,38 @@
-import { Modal, Button, Row } from "react-bootstrap";
-import styles from "../styles/Home.module.scss";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import {useRouter} from "next/router"; 
 
+const Course_Page_ID = "course/[ID]"; 
+const course = [
+    {
+        id: 120,
+        title: "CSC 120"
+    }, 
+    {
+        id: 220, 
+        title: "CSC 220"
+    }
+];
 
-export default function Course() { 
+export default function Home() { 
+    const router = useRouter(); 
+
+    const navigate = (ID) => 
+        router.push({
+            pathname: Course_Page_ID,
+            query: {ID}
+        }); 
+    
     return (
-    <>
-    <head>
-        <title>Silver Surfer</title>
-    </head>
+        <div>
+            <h1>Course Page</h1>
 
-    <Row className="mx-0">
-    <p className={`${styles.text}`}>  This is the course page.</p>
-    </Row>
-   
-    </> 
-    );
+            {course.map((course) => (
+                <div key={`course-${course.id}`}>
+                    <button onClick={() => navigate(course.id)}>{course.title}</button>
+            </div>
+            ))}
+        </div>
+    )
 }
+
 
