f1f28168b3541e8808625243301d0f30bc48ce12
Marisa Stover <stoverme2021@mountunion.edu>
2021-03-03 12:33:12 -0500

    added the remaining professors

--- app/pages/faculty.js
+++ app/pages/faculty.js
@@ -9,6 +9,22 @@ const faculty = [
     {
         name: "Huibregtse",
         title: "Professor Sarah Huibregtse"
+    }, 
+    {
+        name: "Kirchmeyer",
+        title: "Professor John Kirchmeyer"
+    }, 
+    {
+        name: "Klayder",
+        title: "Professor James Klayder"
+    },
+    {
+        name: "Smith",
+        title: "Professor Ken Smith"
+    },
+    {
+        name: "Weber", 
+        title: "Professor Ken Weber"
     }
 
     
