1d458f6ab22e808c8758c26bba73440541d698cd
Marisa Stover <stoverme2021@mountunion.edu>
2021-03-03 12:19:55 -0500

    created majors page

---
+++ app/pages/majors.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import { Modal, Button, Row } from "react-bootstrap";
+import Main from "../layouts/Main/Main";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Silver Surfer</title>
+        
+      </Head>
+
+  
+      <Main />
+
+      <Row>Majors</Row>
+    </>
+  );
+}
