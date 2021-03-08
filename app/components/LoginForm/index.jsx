43a9ef97488528b999293eec68e55bbcf689ab31
Amanda Hegidus <44448511+ach10718@users.noreply.github.com>
2021-03-08 16:26:00 -0500

    working on log in form

---
+++ app/components/LoginForm/index.jsx
@@ -0,0 +1,30 @@
+import {Dropdown} from "react-bootstrap";
+
+export default () => { 
+    return(
+        <Form>
+            <Form.Group as={Row} controlId="formHorizontalEmail">
+                <Form.Label column sm={2}>
+                Email
+                </Form.Label>
+                <Col sm={10}>
+                    <Form.Control type="email" placeholder="Email" />
+                </Col>
+            </Form.Group>
+
+            <Form.Group as={Row} controlId="formHorizontalPassword">
+                <Form.Label column sm={2}>
+                    Password
+                </Form.Label>
+                <Col sm={10}>
+                    <Form.Control type="password" placeholder="Password" />
+                </Col>
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
+    );
+    
+}
