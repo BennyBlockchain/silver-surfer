5b9b4ea0ffbfbb0ba0a6ac28929cccde05b87cb1
Amanda Hegidus <44448511+ach10718@users.noreply.github.com>
2021-03-08 16:52:15 -0500

    Update LoginForm.stories.js

--- app/stories/LoginForm.stories.js
+++ app/stories/LoginForm.stories.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Form from "../components/DropdownMenu"; //import component
+import Form from "../components/LoginForm"; //import component
 
 // template for .storybook
 export default {
