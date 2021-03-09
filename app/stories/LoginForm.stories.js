0e63b4e5b52a5bca980e9a6af9b46a4fe11454ef
Amanda Hegidus <44448511+ach10718@users.noreply.github.com>
2021-03-08 20:29:29 -0500

    Update LoginForm.stories.js

--- app/stories/LoginForm.stories.js
+++ app/stories/LoginForm.stories.js
@@ -3,7 +3,7 @@ import LoginForm from "../components/LoginForm"; //import component
 
 // template for .storybook
 export default {
-    Title: "test",
+    title: "Login Form",
     component: LoginForm,
 };
 
@@ -13,11 +13,11 @@ const LoginFormStory = (props) => <LoginForm {...props} />;
 // example of passing props to storybook component
 export const LoginForm1 = LoginFormStory.bind({});
 LoginForm1.args = {
-  Title: "test1",
+  title: "test1",
 };
 
 // example of passing props to storybook component
 export const LoginForm1 = LoginFormStory.bind({});
 LoginForm2.args = {
-    Title: "test2",
+    title: "test2",
 };
