5af65fc783ab9f7a456a319480ee0c6711b8b0b7
Marisa Stover <stoverme2021@mountunion.edu>
2021-03-23 13:02:25 -0400

    modified based off of mongo branch course/index.js

--- app/pages/course.js
+++ app/pages/course.js
@@ -1,48 +1,14 @@
 import {useRouter} from "next/router"; 
 import Main from "../layouts/Main/Main";
 import Head from "next/head";
+import Link from "next/link";
+import useCourses from "../../hooks/useCourses"; 
 
 
-const Course_Page_ID = "course/[ID]"; 
-const course = [
-    {
-        id: 120,
-        title: "CSC 120"
-    }, 
-    {
-        id: 220, 
-        title: "CSC 220"
-    },
-    {
-        id: 310, 
-        title: "CSC 310"
-    },
-    {
-        id: 320, 
-        title: "CSC 320"
-    },
-    {
-        id: 399, 
-        title: "CSC 399"
-    },
-    {
-        id: 492, 
-        title: "CSC 492"
-    },
-    {
-        id: 360, 
-        title: "CSC 360"
-    }
-];
 
 export default function CoursePage() { 
-    const router = useRouter(); 
 
-    const navigate = (ID) => 
-        router.push({
-            pathname: Course_Page_ID,
-            query: {ID}
-        }); 
+    const courses = useCourses(); 
     
     return (
         <div>
@@ -51,13 +17,16 @@ export default function CoursePage() {
             </Head>
 
         
-            {course.map((course) => (
-                <div key={`course-${course.id}`}>
-                    <button 
-                    onClick={() => navigate(course.id)}>{course.title}
-                    </button>
-                
-            </div>
+            {courses.map((course) => (
+                <Link
+                    href={'course/${course.course_number}'}
+                    key={`key-${course._id}`}
+                    passHref
+                >
+                    <Button key={course._id} className="rounded-circle my-2 p-4 bg-info">
+                    {course.course_number}
+                    </Button>
+                </Link>
             ))}
         
            
