d006e5da5ef6c4146038b1cf7941b7c6924d5745
Amanda Hegidus <44448511+ach10718@users.noreply.github.com>
2021-03-27 21:10:10 -0400

    faculty cards - rest of faculty loaded

--- app/components/FacultyCard/index.jsx
+++ app/components/FacultyCard/index.jsx
@@ -1,13 +1,16 @@
 import {Card} from "react-bootstrap";
 
 
-const FacultyCard = ({title, text, imageName}) => {
+const FacultyCard = ({imageName, name, title, department, office, phone, email}) => {
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={imageName} />
       <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>{text}</Card.Text>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>{title}</Card.Text>
+        <Card.Text>Office: {office}</Card.Text>
+        <Card.Text>Phone: {phone}</Card.Text>
+        <Card.Text>Email: {email}</Card.Text>
       </Card.Body>
     </Card>
   );
