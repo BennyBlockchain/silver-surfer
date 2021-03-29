56ba444baf3c9c0da66af108890109ae461aace8
Benny Blockchain <ben@styng.social>
2021-03-29 15:46:52 -0400

    Sticky course nav

--- app/layouts/Main/Main.jsx
+++ app/layouts/Main/Main.jsx
@@ -2,14 +2,14 @@ import { Container } from "react-bootstrap";
 import MainNav from "../../components/page/MainNav/MainNav";
 import CourseNav from "../../components/page/CourseNav/CourseNav";
 import PageNav from "../../components/page/PageNave/PageNav";
-const Main = ({children}) => {
+const Main = ({ children }) => {
   return (
     <>
-      <Container fluid className="d-flex bg-light vw-100 vh-200 m-0 p-0">
+      <Container fluid className="d-flex bg-light vw-100 h-100 m-0 p-0">
         <CourseNav />
         <PageNav />
         <MainNav />
-        <Container className = "mt-5 mx-5 p-0">{children}</Container>
+        <Container className="mt-5 px-5 p-0">{children}</Container>
       </Container>
     </>
   );
