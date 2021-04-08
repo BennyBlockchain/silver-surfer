aaacad740cac4c1363915309d1c5d83564bc774c
Marisa Stover <stoverme2021@mountunion.edu>
2021-04-08 19:47:59 -0400

    added Buttons and formatting

--- app/pages/admin/index.js
+++ app/pages/admin/index.js
@@ -1,6 +1,11 @@
 import { useSession } from "next-auth/client"; // Get session from provider (ref /pages/_app.js)
 import Head from "next/head";
 import Login from "../../components/Login";
+import MainNav from "../../components/page/MainNav/MainNav";
+import {Container, Row, Col, Button} from "react-bootstrap"; 
+
+
+
 
 /**
  * * If user is not signed in, return the login page.
@@ -15,6 +20,7 @@ export default function admin() {
 
   return (
     <>
+      <MainNav /> 
       <Head>
         <title>Admin | Silver Surfer</title>
       </Head>
@@ -29,6 +35,48 @@ export default function admin() {
           <h1>{`Session token: ${JSON.stringify(session.user)}`}</h1>
         </>
       )}
+      <br /> 
+      <br /> 
+      <br />
+      <center>
+      <h1>Administrative Page</h1>
+      </center>
+      <br /> 
+      <br />
+      <Container> 
+        <Row>
+          <Col>
+          <Button variant="dark">Syllabus</Button>
+          </Col>
+        </Row>
+        <br /> 
+        <Row>
+          <Col>
+          <Button variant="dark">Schedule Updates</Button>
+          </Col>
+        </Row>
+        <br /> 
+        <Row> 
+          <Col>
+          <Button variant="dark">Homework Assignment</Button>
+          </Col>
+        </Row>
+        <br /> 
+        <Row> 
+          <Col>
+          <Button variant="dark">Lab Assignments</Button>
+          </Col>
+        </Row>
+        <br /> 
+        <Row> 
+          <Col>
+          <Button variant="dark">Course Resources</Button>
+          </Col>
+        </Row>
+
+      </Container>
+
+
     </>
   );
 }
