8c20fcd6933c4adb90f6af0b9e5563ef7bf3722c
Benny Blockchain <ben@styng.social>
2021-04-18 14:47:14 -0400

    Mongo API

--- app/pages/api/courses/index.js
+++ app/pages/api/courses/index.js
@@ -1,11 +1,16 @@
 import connectDB from "../../../mongodb/index";
-
 import Courses from "../../../mongodb/models/Courses";
+
 const handler = (req, res) => {
-  Courses.find({}).then((resp) => {
-    console.log(resp);
-    res.send(resp);
-  });
+  Courses.find({})
+    .then((resp) => {
+      console.log(resp);
+      console.log(resp);
+      res.status(202).json(resp);
+    })
+    .catch((err) => {
+      res.send(505).json({ error: err });
+    });
 };
 
 export default connectDB(handler);
