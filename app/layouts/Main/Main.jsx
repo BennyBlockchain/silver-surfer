4ce6756b5540466009f3c0ea4c1980a7c046fc17
stoverme2021 <42615893+stoverme2021@users.noreply.github.com>
2021-04-20 14:36:02 -0400

    Marisa stover (#19)
    * renamed MainNav buttons from Ben to names
    
    * changed lowercase name to uppercase
    
    * adding the rest of the spring semester courses
    
    * Changed circle size and added full course code
    
    * added more padding to menu
    
    * added remaining spring semester courses
    
    * Trying Different Menu Options
    
    * commit
    
    * removed space from line 1
    
    * created course.js file in paths folder
    
    * removed most imports from course page
    
    * made simple formatting changes and added imports
    
    * changed name
    
    * Made a few corrections and simplified
    
    * created [ID] page and course folder
    
    * Deleted course[ID] file
    
    * commit
    
    * created [info].js page to begin dynamic routing
    
    * added dynamic routing lines
    
    * changed css routing
    
    * commit
    
    * changed function name
    
    * updated dynamic document routing
    
    * created csc120.js page
    
    * updated course page + moved it for dynamic routing
    
    * commit
    
    * updating [ID] page for dynamic routing
    
    * updated page
    
    * name changed
    
    * removed doubled file
    
    * commit
    
    * removed files. Unneeded
    
    * user now clicks on course and ID appears
    
    * added additional courses
    
    * updates for course code showing
    
    * added "Course"
    
    * created majors page
    
    * created majors page
    
    * created faculty pages
    
    * added the remaining professors
    
    * created minors page
    
    * added imports
    
    * removed imports
    
    * added links to next.js routing
    
    * removed imports
    
    * container to add content from mongoose branch
    
    * added static content from silver server
    
    * extended body background color
    
    * added static content
    
    * cleaned up
    
    * modified based off of mongo branch course/index.js
    
    * changed responsive elements
    
    * removed commented code
    
    * added faculty card component
    
    * added exceptions
    
    * cleaned up code
    
    * cleaned up
    
    * added Buttons and formatting
    
    * added fields
    
    Co-authored-by: Benny Blockchain <ben@styng.social>

--- app/layouts/Main/Main.jsx
+++ app/layouts/Main/Main.jsx
@@ -5,7 +5,7 @@ import PageNav from "../page/PageNave/PageNav";
 const Main = ({ children }) => {
   return (
     <>
-      <Container fluid className="d-flex bg-light vw-100 h-100 m-0 p-0">
+      <Container fluid className="d-flex bg-light vw-100 vh-100 m-0 p-0">
         <CourseNav />
         {/* <PageNav /> */}
         <MainNav />
--- app/pages/admin/index.js
+++ app/pages/admin/index.js
@@ -1,7 +1,12 @@
 import { useSession } from "next-auth/client"; // Get session from provider (ref /pages/_app.js)
 import Head from "next/head";
 import Login from "../../components/Login";
-import LoginForm from "../../components/LoginForm";
+import MainNav from "../../components/page/MainNav/MainNav";
+import {Container, Row, Col, Button} from "react-bootstrap"; 
+import React, {useState} from 'react'; 
+
+
+
 
 /**
  * * If user is not signed in, return the login page.
@@ -11,19 +16,23 @@ import LoginForm from "../../components/LoginForm";
 export default function admin() {
   // Hook for getting session info
   const [session, loading] = useSession();
+  const [fileUploadActive, setFileUploadActive] = useState(false);
+  const [scheduleChangeActive, setScheduleChangeActive] = useState(false); 
+  const [homeworkChangeActive, setHomeworkChangeActive] = useState(false); 
+  const [labChangeActive, setLabChangeActive] = useState(false); 
+  const [resourceChangeActive, setResourceChangeActive] = useState(false); 
+
   // SSR check for session
   if (typeof window !== "undefined" && loading) return null;
 
+  
+
   return (
     <>
+      <MainNav /> 
       <Head>
         <title>Admin | Silver Surfer</title>
       </Head>
-
-      TODO: Put login react component here.
-
-      <br />
-
       {!session && (
         <>
           <Login />
@@ -35,6 +44,126 @@ export default function admin() {
           <h1>{`Session token: ${JSON.stringify(session.user)}`}</h1>
         </>
       )}
+      <br /> 
+      <br /> 
+      <br />
+      <center>
+      <h1>Administrative Page</h1>
+      </center>
+      <br /> 
+      <br />
+      <Container> 
+        <Row>
+          <Col>
+          <Button variant="dark" onClick={() => setFileUploadActive(true)}>Syllabus</Button>
+          
+          </Col>
+          <Col>
+          {fileUploadActive && <input type="file"/>}
+          <br/> 
+          <br/> 
+          {scheduleChangeActive && <form>
+            <label>
+              Title:
+            </label>
+            <input type="text"></input>
+            <br/>
+            <label>
+              Date:
+            </label>
+            <input type="text"></input>
+            <br/> 
+            <label>
+              Description: 
+            </label>
+            <input type="textarea"></input>
+            </form>}
+            <br/> 
+          {homeworkChangeActive && <form>
+            <label>
+              Title:
+            </label>
+            <input type="text"></input>
+            <br/>
+            <label>
+              Date:
+            </label>
+            <input type="text"></input>
+            <br/> 
+            <label>
+              Description: 
+            </label>
+            <input type="textarea"></input>
+            </form>}
+            <br/> 
+            {labChangeActive && <form>
+            <label>
+              Title:
+            </label>
+            <input type="text"></input>
+            <br/>
+            <label>
+              Date:
+            </label>
+            <input type="text"></input>
+            <br/> 
+            <label>
+              Description: 
+            </label>
+            <input type="textarea"></input>
+            </form>}
+            <br/> 
+            {resourceChangeActive && <form>
+            <label>
+              Title:
+            </label>
+            <input type="text"></input>
+            <br/>
+            <label>
+              Date:
+            </label>
+            <input type="text"></input>
+            <br/> 
+            <label>
+              Description: 
+            </label>
+            <input type="textarea"></input>
+            <br/> 
+            <label>
+              Links: 
+            </label>
+            <input type="text"></input>
+            </form>}
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+          <Button variant="dark" onClick={() => setScheduleChangeActive(true)}>Schedule Updates</Button>
+          </Col>
+        </Row>
+        <br/> 
+        <Row> 
+          <Col>
+          <Button variant="dark" onClick={() => setHomeworkChangeActive(true)}>Homework Assignment</Button>
+          </Col>
+        </Row>
+        <br/> 
+        <Row> 
+          <Col>
+          <Button variant="dark" onClick={() => setLabChangeActive(true)}>Lab Assignments</Button>
+          </Col>
+        </Row>
+        <br/> 
+        <Row> 
+          <Col>
+          <Button variant="dark" onClick={() => setResourceChangeActive(true)}>Course Resources</Button>
+          </Col>
+        </Row>
+
+      </Container>
+
+
     </>
   );
 }
